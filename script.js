99ed02ca5e332ffb819b7d283b222d9dcd7ea19a
Patrick Moreau <patrick@inscribe.ai>
2023-01-07 15:17:00 +0000

    new format

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const header = document.getElementById('header');
 
-let x = 0;
-let y = 0;
+
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
 
 function updatePosition(event) {
   const dx = event.clientX - x;
   const dy = event.clientY - y;
-  x += dx * 0.05;
-  y += dy * 0.05;
-  header.style.top = x + 'px';
-  header.style.left = y + 'px';
+  x -= dx * 0.009;
+  y -= dy * 0.009;
+
+  // prevent the header from moving outside the boundaries of the page
+  x = Math.max(0, Math.min(x, window.innerWidth - header.offsetWidth));
+  y = Math.max(0, Math.min(y, window.innerHeight - header.offsetHeight));
+
+  header.style.top = y + 'px';
+  header.style.left = x + 'px';
 }
 
 window.addEventListener('mousemove', updatePosition)
