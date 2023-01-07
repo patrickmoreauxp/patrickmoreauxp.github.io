7586558b93d474217440a4f95f0c887c144925b3
Patrick Moreau <patrick@inscribe.ai>
2023-01-07 13:41:54 +0000

    new format

---
+++ script.js
@@ -0,0 +1,15 @@
+const header = document.getElementById('header');
+
+let x = 0;
+let y = 0;
+
+function updatePosition(event) {
+  const dx = event.clientX - x;
+  const dy = event.clientY - y;
+  x += dx * 0.2;
+  y += dy * 0.2;
+  header.style.top = x + 'px';
+  header.style.left = y + 'px';
+}
+
+window.addEventListener('mousemove', updatePosition);
